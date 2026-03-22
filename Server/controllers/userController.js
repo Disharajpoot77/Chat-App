abe8729d0d815bce63980d4529e7f0c2d2f67b17
Disha rajpoot <rajpootdisha42@gmail.com>
2026-03-22 21:56:43 +0530

    user

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -4,7 +4,7 @@ exports.getUsers = async (req, res) => {
   try {
     const users= await User.find({}, "username");
     res.json(users);
-  } catch (err) {
+  } catch(err) {
     console.log("Users error:", err);
     res.status(500).json({ message: "Server error" });
   }
