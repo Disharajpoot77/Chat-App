4f77d63d8ce75171df518a85d5f2a8249dba5ec7
Disha rajpoot <rajpootdisha42@gmail.com>
2026-03-28 21:56:08 +0530

    db

--- Server/config/db.js
+++ Server/config/db.js
@@ -4,7 +4,7 @@ function db(){
 mongoose.connect("mongodb://127.0.0.1:27017/chatApp").then(()=>{
 	console.log("DB connected")
 }).catch((err)=>{
-	console.log(err);
+	 console.log(err);
 })
 }
 module.exports =db
