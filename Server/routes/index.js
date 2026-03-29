7fb8e805d7f3622d952972578518a20b4fce100d
Disha rajpoot <rajpootdisha42@gmail.com>
2026-03-29 22:21:15 +0530

    index

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { signup, login } = require("../controllers/authController");
-const { getUsers } = require("../controllers/userController");
+const { signup, login } = require("./controllers/authController");
+const { getUsers } = require("./controllers/userController");
 const auth = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.post("/signup", signup);
+ router.post("/signup", signup);
 router.post("/login", login);
 router.get("/users", auth, getUsers);
 
